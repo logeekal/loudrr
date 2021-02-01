32bf9cd3090076fd8a8152bd40c4fe80f1e5c882
Jatin Kathuria <jtn.kathuria@gmail.com>
2021-02-01 07:12:11 +0100

    Password hashing done

--- api/passport/local.js
+++ api/passport/local.js
@@ -1,13 +1,27 @@
 const LocalStrategy = require("passport-local").Strategy;
 const dbAdapter = require("../db/dbAdapter");
+const bcrypt =  require('bcrypt');
 
 const localStrategy = new LocalStrategy(
   { usernameField: "email", passwordField: "password" },
   async (username, password, done) => {
     try {
-      const loggedInUser = await dbAdapter.loginUser(username, password);
-      return done(null, loggedInUser);
+      const existingUser = await dbAdapter.getUser(username);
+      if(!existingUser){
+        console.log('existing user does not exists')
+        return(null, false, {message: "User does not exist"});
+      }
+      const isValidPassword = await bcrypt.compare(password, existingUser.password);
+
+      if(!isValidPassword){
+        console.log('Not a valid Password')
+        throw new Error('Invalid Credentials. Username and password do not match');
+      }
+      const { password: userPassword, ...userWithoutPassword}  = existingUser;
+      return done(null, userWithoutPassword);
     } catch (err) {
+
+      console.log(err)
       return done(null, false, { message: err.message });
     }
   }
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dbAdapter = require("../db/dbAdapter");
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
@@ -18,8 +19,15 @@ router.post("/create", async (req, res) => {
   if (!email || !password || !name) {
     res.status(400).send("Mandatory field missing");
   }
+
+  if(password.length < 8){
+    res.status(400).send('Password should be at least 8 characters long.')
+  }
+
+  const passwordHash = bcrypt.hashSync(password,2);
+
   try {
-    const createdUser = await dbAdapter.createUser(email,name,password,'avatar');
+    const createdUser = await dbAdapter.createUser(email,name,passwordHash,'avatar');
     res.send(200).send(createdUser);
   } catch (err) {
     res.status(500).send(err);
